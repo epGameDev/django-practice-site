b33c61274e82661f66bdc70f8ecd2d668ff90281
Eric Philippot <EricpDev@gmail.com>
2024-08-08 17:26:49 -0500

    tested dynamic views

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,5 +4,5 @@ from . import views; # The dot states "from this module or folder"
 
 urlpatterns = [
   path('', views.index),
-  path("<month>/", views.monthly_challenge )
+  path("<str:month>/", views.monthly_challenge ),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render;
 from django.http import HttpResponse, HttpResponseNotFound;
 
+monthly_challenges = {
+"january": "Learn Design, HTML, and CSS",
+"february": "Learn Javascript",
+"march": "Learn TypeScript",
+"april": "Learn React and Redux",
+"may": "Learn SQL and PostgreSQL",
+"june": "Learn Docker and Kubernetes",
+"july": "Learn Algorithms and Data Structures",
+"august": "Learn Python and Automation",
+"september": "Learn Linux Administration",
+"october": "Learn Golang",
+"november": "Learn Django",
+"december": "Learn Rust",
+}
+
+
 # Create your views here.
 def index(request):
   return HttpResponse("Hello World!");
 
 def monthly_challenge(request, month):
-  return HttpResponse();
+
+  try:
+    challenge_text = monthly_challenges[month];
+    return HttpResponse(f'Your challenge for the money: {challenge_text}');
+  except:
+    return HttpResponseNotFound("This page you are looking for is not found. Check your spelling or contact the site support.");
